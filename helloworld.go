370b5f40a8bfedb6e692572ac58ece7ef0b0b022
keviny22 <kevin_young@intuit.com>
2015-11-10 12:45:03 -0800

    Initial

---
+++ helloworld.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	//var message string = "hello go"
+	//var message = "hello go"
+	message := "Hello Go"
+	//var a, b, c int = 1, 2, 3
+	//var a, b, c  = 1, 2, 3
+	a, b, c := 1, false, "foo"
+	//a = 1
+	//message = "hello go world"
+	fmt.Println(message, a, b, c)
+}
