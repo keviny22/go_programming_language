70faf2ae4f234a7d3dbe9a0980ffc8a766c6a5a5
keviny22 <kevin_young@intuit.com>
2015-11-12 15:41:41 -0800

    Initial

--- greeting/greeting.go
+++ greeting/greeting.go
@@ -4,28 +4,11 @@ import (
 	"fmt"
 )
 
-//type Salutation string
+type Printer func(string)
 
 type Salutation struct {
-	name1     string
-	greeting1 string
-}
-
-func main() {
-	message := "Hello Go"
-	var greeting *string = &message
-
-	fmt.Println(message, greeting)
-	fmt.Println(&message, *greeting)
-	fmt.Println(message, *greeting)
-
-	//reassign memory address for greeting and message to a different value
-	*greeting = "hi"
-
-	var s = Salutation{"Joe", "hello"}
-	s.name1 = "Bob"
-
-	Greet(s, CreatePrintFunction("!!!!"), true)
+	Name1     string
+	Greeting1 string
 }
 
 //both are strings as input, output is also returns two strings
@@ -36,18 +19,43 @@ func CreateMessage(name, greeting string) (message string, alternate string) {
 
 }
 
-type Printer func(string)
+func Greet(salutation []Salutation, do Printer, isFormal bool, times int) {
 
-func Greet(salutation Salutation, do Printer, isFormal bool) {
+	//loop through ever itme in the solutation colletion(slice)
+	for _, s := range salutation {
+		message, alternate := CreateMessage(s.Name1, s.Greeting1)
 
-	message, alternate := CreateMessage(salutation.name1, salutation.greeting1)
-
-	if isFormal {
-		do(message)
+		if prefix := GetPrefix(s.Name1); isFormal {
+			do(prefix + message)
+		} else {
+			do(alternate)
+		}
 	}
 
-	do(alternate)
+}
 
+func GetPrefix(name string) (prefix string) {
+
+	//map with key of string and value of string type
+	var prefixMap map[string]string
+	prefixMap = make(map[string]string)
+
+	prefixMap["Bob"] = "Mr "
+	prefixMap["Joe"] = "Dr "
+	prefixMap["Mary"] = "Mrs "
+	prefixMap["Amy"] = "Dr "
+
+	switch {
+	case name == "Bob":
+		prefix = "Mr "
+	case name == "Joe", name == "Amy", len(name) == 10:
+		prefix = "Dr "
+	case name == "Mary":
+		prefix = "Mrs "
+	default:
+		prefix = "Dude "
+	}
+	return
 }
 
 func Print(s string) {
@@ -65,4 +73,20 @@ func CreatePrintFunction(custom string) Printer {
 }
 func PrintCustom(s string, custom string) {
 	fmt.Println(s + custom)
+}
+
+//create a empty interface of any type, return the type
+func TypeSwitchTest(x interface{}) {
+	switch t := x.(type) {
+	case int:
+		fmt.Println("int")
+	case string:
+		fmt.Println("string")
+	case Salutation:
+		fmt.Println("salutation")
+	default:
+		fmt.Println("unknown")
+		_ = t
+
+	}
 }
--- helloworld.go
+++ helloworld.go
@@ -1,114 +1,16 @@
 package main
 
 import (
-	"fmt"
-)
-
-//type Salutation string
-
-type Salutation struct {
-	name1     string
-	greeting1 string
-}
-
-const (
-	D = iota
-
-	PI       = 3.14 //types are inferred
-	Language = "Go"
-	//iota is successvie untyped integer constangs
-	A = iota
-	B = iota
-	C = iota
-	// becuase you dont declare, it will assume for previous
-	E = iota
-	G
-	F
-)
-
-const (
-	//new const will set iota to zero
-	H = iota
-	I
-	J
+	"./greeting"
 )
 
 func main() {
-	//var message string = "hello go"
-	//var message = "hello go"
-	message := "Hello Go"
-	var greeting *string = &message
+	//var s = greeting.Salutation{"Bob", "Hello"}
 
-	fmt.Println(message, greeting)
-	fmt.Println(&message, *greeting)
-	fmt.Println(message, *greeting)
-
-	//reassign memory address for greeting and message to a different value
-	*greeting = "hi"
-	fmt.Println(message, *greeting)
-
-	//var message1 Salutation = "Hello World"
-	//fmt.Println(message1)
-
-	var s = Salutation{"Joe", "hello"}
-	s.name1 = "Bob"
-	fmt.Println(s.name1)
-	fmt.Println(s.greeting1)
-
-	fmt.Println(PI)
-	fmt.Println(Language)
-	fmt.Println(A, B, C, D, E, F, G, H, I, J)
-
-	//var a, b, c int = 1, 2, 3
-	//var a, b, c  = 1, 2, 3
-	//a, b, c := 1, false, "foo"
-	//a = 1
-	//message = "hello go world"
-	//fmt.Println(message, a, b, c)
-
-	Greet(s, CreatePrintFunction("!!!!"), true)
-}
-
-//both are strings as input, output is also returns two strings
-func CreateMessage(name, greeting string) (message string, alternate string) {
-	//fmt.Println(len(greeting))
-	message = greeting + " " + name
-	alternate = "Hey " + name
-	return
-
-	//fmt.Println(name, greeting)
-}
-
-type Printer func(string)
-
-//func Greet(salutation Salutation, do func(string)) {
-func Greet(salutation Salutation, do Printer, isFormal bool) {
-	//fmt.Println(salutation.name1)
-	//fmt.Println(salutation.greeting1)
-	//fmt.Println(CreateMessage(salutation.name1, salutation.greeting1))
-	message, alternate := CreateMessage(salutation.name1, salutation.greeting1)
-
-	if isFormal {
-		do(message)
+	slice := []greeting.Salutation{
+		{"Bob", "hello"},
+		{"Joe", "hi"},
+		{"Mary", "whatup"},
 	}
-
-	do(alternate)
-
-}
-
-func Print(s string) {
-	fmt.Print(s)
-}
-
-func PrintLine(s string) {
-	fmt.Println(s)
-}
-
-func CreatePrintFunction(custom string) Printer {
-	return func(s string) {
-		fmt.Println(s + custom)
-	}
-}
-func PrintCustom(s string, custom string) {
-	fmt.Println(s + custom)
+	greeting.Greet(slice, greeting.CreatePrintFunction("?"), true, 6)
 }
